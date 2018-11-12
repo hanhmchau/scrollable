7b15a59824d88299b194bbc57665f697fa6c06a1
M.C <hanhchaumn@gmail.com>
2018-11-12 17:25:29 +0700

    Add constants

---
+++ server/consts/index.ts
@@ -0,0 +1,11 @@
+const consts = {
+    COLUMN_INDEX: {
+        END_OF_DAY: 4
+    },
+    QUANDL_ERRORS: {
+        INCORRECT_PARAMS: 'QESx04',
+        QUANDL_CODE_NOT_FOUND: 'QECx02'
+    }
+};
+
+export default consts;
