8e101b0022386b7495f308cab8eda6fce887e3c0
M.C <hanhchaumn@gmail.com>
2018-11-13 10:52:05 +0700

    Add caching to get closing price

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,11 +1,22 @@
 import axios from 'axios';
-import { addDays, format, parse } from 'date-fns';
+import {
+    addDays,
+    format,
+    parse,
+    isWithinRange,
+    eachDay,
+    differenceInCalendarDays,
+    isEqual
+} from 'date-fns';
 import consts from '../consts';
 import Error from '../models/error';
+import * as NodeCache from 'node-cache';
 const dateFormat = 'YYYY-MM-DD';
 const quandlUrl = 'https://www.quandl.com/api/v3/datasets/WIKI';
+const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 export const createError = (e: any) => {
+    console.log(e);
     const data = e.response.data;
     const message = data.quandl_error.message;
     delete data.quandl_error;
@@ -24,7 +35,27 @@ const getEndOfDays = async (
     startDate: Date | string,
     endDate: Date | string
 ) => {
-    return axios.get(`${quandlUrl}/${ticker}/data.json`, {
+    const cachedResult: any = cache.get(ticker);
+    const start = format(startDate, dateFormat);
+    const end = format(endDate, dateFormat);
+    if (cachedResult) {
+        const data = [];
+        for (const res in cachedResult) {
+            if (cachedResult.hasOwnProperty(res)) {
+                if (isWithinRange(res, start, end)) {
+                    data.push([res, cachedResult[res].value]);
+                }
+            }
+        }
+        if (differenceInCalendarDays(end, start) + 1 === data.length) {
+            return {
+                startDate: data[0][0],
+                endDate: data[data.length - 1][0],
+                data: data.filter(d => d[1] !== -1)
+            };
+        }
+    }
+    const response = await axios.get(`${quandlUrl}/${ticker}/data.json`, {
         params: {
             start_date: formatDate(startDate),
             end_date: formatDate(endDate),
@@ -33,6 +64,23 @@ const getEndOfDays = async (
             column_index: consts.COLUMN_INDEX.END_OF_DAY
         }
     });
+
+    const newCacheResult: any = cachedResult || {};
+    const dataset = response.data.dataset_data;
+    const dat = dataset.data;
+    const receivedStart = format(dataset.start_date, dateFormat);
+    const receivedEnd = format(dataset.end_date, dateFormat);
+    let i = 0;
+    eachDay(startDate || receivedStart, endDate || receivedEnd).forEach(day => {
+        if (isEqual(dat[i], day)) {
+            newCacheResult[format(day, dateFormat)] = dat[i][1];
+            i++;
+        } else {
+            newCacheResult[format(day, dateFormat)] = -1;
+        }
+    });
+    cache.set(ticker, newCacheResult);
+    return response.data.dataset_data;
 };
 
 export const movingDayAverage = async (
@@ -76,10 +124,9 @@ export const movingDayAverage = async (
 export const closePrice = async (
     ticker: string,
     startDate: Date | string,
-    endDate: Date
+    endDate: Date | string
 ) => {
-    const result = await getEndOfDays(ticker, startDate, endDate);
-    const dataset = result.data.dataset_data;
+    const dataset = await getEndOfDays(ticker, startDate, endDate);
     return {
         startDate: dataset.start_date,
         endDate: dataset.end_date,
