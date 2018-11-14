19952f700fad0259e3a17815afaf27849c2ff6a7
M.C <hanhchaumn@gmail.com>
2018-11-14 16:53:04 +0700

    Implement new client service

---
+++ src/app/services/ticker.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { format, parse } from 'date-fns';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import consts from '../../consts';
+import GraphInput, { GraphSeries } from '../models/graph-input';
+import Ticker from '../models/ticker';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TickerService {
+    constructor(private http: HttpClient) {}
+
+    getClosePrice(
+        symbol: string,
+        startDate: Date,
+        endDate: Date
+    ): Observable<GraphInput> {
+        let params = new HttpParams();
+        if (startDate) {
+            params = params.set(
+                'startDate',
+                format(startDate, consts.DATE_FORMAT)
+            );
+        }
+        if (endDate) {
+            params = params.set('endDate', format(endDate, consts.DATE_FORMAT));
+        }
+        return this.http
+            .get<Ticker[]>(`/api/${symbol}/close-price`, {
+                params
+            })
+            .pipe(
+                catchError(err => of(undefined)),
+                map((data: any) => {
+                    if (data) {
+                        const dataset: any[][] = data.prices.dateClose.data;
+                        const graphInput = new GraphInput();
+                        const graphSeries = new GraphSeries();
+                        graphSeries.name = symbol;
+                        dataset.forEach(el => {
+                            graphSeries.series.push({
+                                label: symbol,
+                                name: parse(el[0] as string),
+                                value: el[1] as number
+                            });
+                        });
+                        graphInput.data.push(graphSeries);
+                        return graphInput;
+                    }
+                })
+            );
+    }
+
+    searchTickers(search: string): Observable<Ticker[]> {
+        const tickerSearchStr = localStorage.getItem(
+            consts.CACHE.TICKER_SEARCH
+        );
+        const cache: any = tickerSearchStr ? JSON.parse(tickerSearchStr) : {};
+        const tickersWithThisSearch = cache[search];
+
+        if (tickersWithThisSearch) {
+            return of(tickersWithThisSearch);
+        }
+        return this.queryTickers(search).pipe(
+            tap(tickers => {
+                cache[search] = tickers;
+                localStorage.setItem(
+                    consts.CACHE.TICKER_SEARCH,
+                    JSON.stringify(cache)
+                );
+            })
+        );
+    }
+
+    getFullName(symbol: string): Observable<any> {
+        return this.http
+            .get<any>(`/api/${symbol}/full-name`);
+    }
+
+    private queryTickers(search: string): Observable<Ticker[]> {
+        if (!search) {
+            return of([]);
+        }
+        const params = new HttpParams().set('search', search).set('top', '5');
+        return this.http.get<Ticker[]>('/api/tickers', {
+            params
+        });
+    }
+}
