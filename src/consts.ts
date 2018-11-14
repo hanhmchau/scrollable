67211c823e390a2ba9159dfb99ab559955cd0114
M.C <hanhchaumn@gmail.com>
2018-11-14 16:53:16 +0700

    Create constant values

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,9 @@
 const consts = {
-    MAP_API: 'AIzaSyCDXBrp0WaDo4gmD2YhvILvgAq4cmLnAv4',
-    FIREBASE: {
-        apiKey: 'AIzaSyCDXBrp0WaDo4gmD2YhvILvgAq4cmLnAv4',
-        authDomain: 'firemap-219503.firebaseapp.com',
-        databaseURL: 'https://firemap-219503.firebaseio.com',
-        projectId: 'firemap-219503',
-        storageBucket: 'firemap-219503.appspot.com',
-        messagingSenderId: '437193568676'
-    },
-    GEONAME_USER: 'luxa',
-    GEONAME_LEVELS: {
-        CITY: 'ADM1',
-        COUNTRY: 'PCLI',
-        DISTRICT: 'ADM2',
-        WARD: 'ADM3'
-    },
-    NUMBERS: ['Một', 'Hai', 'Ba', 'Bốn', 'Năm', 'Sáu', 'Bảy', 'Tám', 'Chín', 'Mười', 'Mười Một', 'Mười Hai'],
-    GEONAME_ID: {
-        HO_CHI_MINH_CITY: 1580578
-    },
     CACHE: {
-        COUNTRIES: 'countries'
-    }
+        TICKER_SEARCH: 'TICKER_SEARCH'
+    },
+    CURRENT_DAY: new Date('2018-03-27'),
+    DATE_FORMAT: 'YYYY-MM-DD'
 };
 
 export default consts;
