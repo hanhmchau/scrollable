1cb9cfc2796cafc5b10e3d0c45540a7121909954
M.C <hanhchaumn@gmail.com>
2018-11-14 17:22:50 +0700

    Create search container

---
+++ src/app/search-container/search-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-search-container',
+    templateUrl: './search-container.component.html',
+    styleUrls: ['./search-container.component.css']
+})
+export class SearchContainerComponent {
+}
