a94cb2120e062d09069cc4889b089a24d78c1fc4
M.C <hanhchaumn@gmail.com>
2018-11-15 09:52:05 +0700

    Add 404 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchContainerComponent } from './search-container/search-container.component';
 import { GraphComponent } from './graph/graph.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
     {
@@ -12,7 +13,8 @@ const routes: Routes = [
     {
         path: 'ticker/:symbol',
         component: GraphComponent
-    }
+    },
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
