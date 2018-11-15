e848f6bf877ee12a978927777655725597116137
M.C <hanhchaumn@gmail.com>
2018-11-15 16:05:40 +0700

    Enable multiple mode in search box

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+    ElementRef
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatAutocompleteSelectedEvent } from '@angular/material';
+import {
+    MatAutocomplete,
+    MatAutocompleteSelectedEvent,
+    MatChipInputEvent
+} from '@angular/material';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import {
+    debounceTime,
+    distinctUntilChanged,
+    map,
+    switchMap
+} from 'rxjs/operators';
 import Ticker from '../models/ticker';
 import { TickerService } from './../services/ticker.service';
 
@@ -15,7 +32,18 @@ import { TickerService } from './../services/ticker.service';
 export class SearchComponent {
     myControl = new FormControl();
     filteredOptions: Observable<Ticker[]>;
+    selectedTickers: string[] = [];
+    separatorKeysCodes: number[] = [ENTER, COMMA];
+    removable = true;
+    selectable = true;
+    @Input() width = '500px';
+    @Input() placeholder = 'Input a ticker here...';
+    @Input() multiple = false;
+    @Input() dontShowThis: string[] = [];
     @Output() onTickerSelected = new EventEmitter<string>();
+    @Output() onTickerDeleted = new EventEmitter<string>();
+    @ViewChild('auto') matAutocomplete: MatAutocomplete;
+    @ViewChild('input') input: ElementRef<HTMLInputElement>;
 
     constructor(private tickerService: TickerService, private router: Router) {}
 
@@ -23,11 +51,41 @@ export class SearchComponent {
         this.filteredOptions = this.myControl.valueChanges.pipe(
             debounceTime(150),
             distinctUntilChanged(),
-            switchMap(value => this.tickerService.searchTickers(value))
+            switchMap(value => this.tickerService.searchTickers(value)),
+            map(tickers =>
+                tickers.filter(
+                    t =>
+                        !this.selectedTickers.includes(t.symbol) &&
+                        !this.dontShowThis.includes(t.symbol)
+                )
+            )
         );
     }
 
     tickerSelected(event: MatAutocompleteSelectedEvent) {
-        this.onTickerSelected.emit(event.option.value);
+        const ticker = event.option.value;
+        if (this.multiple) {
+            this.selectedTickers.push(ticker);
+        }
+        this.onTickerSelected.emit(ticker);
+        this.clearInput();
+    }
+
+    remove(ticker: string) {
+        this.selectedTickers = this.selectedTickers.filter(t => t !== ticker);
+        this.onTickerDeleted.emit(ticker);
+    }
+
+    add(event: MatChipInputEvent): void {
+        // Add fruit only when MatAutocomplete is not open
+        // To make sure this does not conflict with OptionSelected Event
+        if (!this.matAutocomplete.isOpen) {
+            this.clearInput();
+        }
+    }
+
+    clearInput(): void {
+        this.input.nativeElement.value = '';
+        this.myControl.setValue(null);
     }
 }
