0b3c6f41e61b2abdfe4b416131f6cb90d3719ff4
M.C <hanhchaumn@gmail.com>
2018-11-15 16:04:38 +0700

    Add comparison autocomplete

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatChipsModule
 } from '@angular/material';
 
 const modules = [
@@ -22,7 +23,8 @@ const modules = [
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatChipsModule
 ];
 
 @NgModule({
