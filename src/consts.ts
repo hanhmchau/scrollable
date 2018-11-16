7bd9dce8660b1f295e38eb7b785ecfad9a94fb82
M.C <hanhchaumn@gmail.com>
2018-11-16 13:44:24 +0700

    Add constants

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,10 @@ const consts = {
         TICKER_SEARCH: 'TICKER_SEARCH'
     },
     CURRENT_DAY: new Date('2018-03-27'),
-    DATE_FORMAT: 'YYYY-MM-DD'
+    DATE_FORMAT: 'YYYY-MM-DD',
+    ERROR: {
+        TICKER_NOT_FOUND: 'TICKER_NOT_FOUND'
+    }
 };
 
 export default consts;
