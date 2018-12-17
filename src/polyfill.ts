958c09c23bead555ccc8b1f74074e66aaaa18b6a
M.C <hanhchaumn@gmail.com>
2018-12-17 11:17:26 +0700

    Add polyfill

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,2 +1,8 @@
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'core-js';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        (Element.prototype as any).msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
+}
